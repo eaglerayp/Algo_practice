039ac3816c7452b76b647785ef3a980f55dae8d1
Yuan <R02725014@ntu.edu.tw>
2014-07-20 13:04:13 +0800

    refactoring

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+
 
 public class Main
 {
@@ -16,7 +8,7 @@ public class Main
    {
 	   int [] inputnotsort={1,3,3,5,7,9,9,13};//
 
-	   int[]count=countingsort.countingsort(inputnotsort);
+	   int[]count=testfunction.countingsort(inputnotsort);
 	   System.out.println("sort array:");
 	   for(int i=0;i<count.length;i++){
 		   int x=count[i];
@@ -24,7 +16,7 @@ public class Main
 	   }
 	   System.out.println();
 	   
-	   int[]dup=countingsort.findduplicate(inputnotsort);
+	   int[]dup=testfunction.findduplicate(inputnotsort);
 	   for(int i=0;i<dup.length;i++){
 		   System.out.println("DUP"+dup[i]);
 	   }
---
+++ testfunction.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+
+public class testfunction {
+	   public static int[] countingsort(int[] input){
+		   int max=0;
+		   int[] result=new int[input.length];
+		   for(int i=0;i<input.length;i++){  //find max
+			    int c = input[i] - max;//a-b
+			    int k = (c >> 31) & 1;//bit: 0 or 1
+			    max = input[i] - k * c; //a - 0*(a-b)=a  or a-1(a-b) = b
+		   }
+		   max++;
+		   int[] count=new int[max];
+		   for(int i=0;i<input.length;i++){  // can find duplicate element if  "count>1"
+			   count[input[i]]++;
+		   }
+		   int total=0;
+		   for(int i=0;i<max;i++){
+			   int temp=count[i];
+			   count[i]=total;
+			   total+=temp;
+		   }
+		   for(int i=0;i<input.length;i++){
+			   int x=input[i];
+			   result[count[x]]=x;
+			   count[x]++;		   
+		   }
+		   return result;
+	   }
+	   public static int[] findduplicate(int[] input){
+		   int max=0;
+		   ArrayList<Integer> result=new ArrayList<Integer>();
+		   for(int i=0;i<input.length;i++){  //find max
+			    int c = input[i] - max;//a-b
+			    int k = (c >> 31) & 1;//bit: 0 or 1
+			    max = input[i] - k * c; //a - 0*(a-b)=a  or a-1(a-b) = b
+		   }
+		   max++;
+		   int[] count=new int[max];
+		   for(int i=0;i<input.length;i++){  // can find duplicate element if  "count>1"
+			   count[input[i]]++;
+			   if(count[input[i]]>1){
+				   result.add(input[i]);
+			   }
+		   }
+		   int[]dups=new int[result.size()];
+		   for(int i=0;i<result.size();i++){
+			   dups[i]=result.get(i);
+		   }
+		   result.clear();
+		   return dups;
+	   }
+}
