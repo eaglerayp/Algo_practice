0225f4192c15c01b05fb8ae94c7281594f801e13
eaglerayp <eagle_rayp@hotmail.com>
2014-08-13 20:35:43 +0800

    Update plateau.java

--- plateau.java
+++ plateau.java
@@ -1,6 +1,6 @@
 
 public class plateau {
-	public static int findlongestlength(int[] input){ //find longest same integer's length
+    public static int findlongestlength(int[] input){ //find longest same integer's length
 		int maxlen=0;
 		for(int i=0;i<input.length;i++){
 			if(input[i]==input[i-maxlen]){
@@ -8,7 +8,7 @@ public class plateau {
 			}
 		}
 		return maxlen;
-	}
+    }
     public static int findlongestelement(int[] input){ //find longest same integer
         int maxlen=0;
         int maxindex=0;
