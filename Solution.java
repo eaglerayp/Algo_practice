ea174f902f97138c919fcaf37cdb8ece3d52b967
Yuan <R02725014@ntu.edu.tw>
2015-02-17 00:35:26 +0800

    add garbage problem find first positive missing element

--- Solution.java
+++ Solution.java
@@ -9,9 +9,42 @@ public class Solution {
         for (int i : A) result ^= i ;
         return result ;
     }
+    public static int firstMissingPositive(int[] A) {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        int answer=-99;
+        if(A.length==0){
+            answer=1;
+        }
+        else {
+            for(int i:A){
+                if(i>0) {
+                    if (i > max)
+                        max = i;
+                    if (i < min)
+                        min = i;
+                }
+            }
+            boolean[] counting=new boolean[max+2];
+            for(int i:A){
+                if(i>0) {
+                    counting[i] = true;
+                }
+            }
+            for(int i=1;i<=max+2;i++){
+                if(!counting[i]) {
+                    answer=i;
+                    break;
+                }
+
+            }
+        }
+        return answer;
+    }
     public static void main(String [] args)    {
-        int test[]={2,2,3,6,6,5,5,7,7};
-        int n=singleNumber(test);
+        int singletest[]={2,2,3,6,6,5,5,7,7};
+        int firstMissingPositivetest[]={1};
+        int n=firstMissingPositive(firstMissingPositivetest);
         double b=4.0;
         System.out.println(n);
        // System.out.println(5^10^5);
