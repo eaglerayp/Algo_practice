a8fe086976900835da1f21930be33f91dda08a9b
eaglerayp <eagle_rayp@hotmail.com>
2015-06-16 10:30:24 +0800

    add invert binary tree ,ya can go google

--- Solution.java
+++ Solution.java
@@ -568,6 +568,16 @@ public class Solution {
         }
         return result;
     }
+    public TreeNode invertTree(TreeNode root) {
+        if(root!=null){
+            TreeNode temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
     public static void main(String [] args)    {
         System.out.println(convert("PAYPALISHIRING", 3));
        // int singletest[]={2,2,3,6,6,5,5,7,7};
